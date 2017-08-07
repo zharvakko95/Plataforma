e7f2d5e153acc5ff935d66d6997be2ab6b6a7d47
Alan Rincón <alan.rnca@gmail.com>
2017-08-07 17:23:39 -0500

    Merge branch 'Develop' of https://github.com/zharvakko95/Plataforma into Develop

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home')->with('grades', Grade::all('id','name'));
+        return view('home')->with('grades', Grade::all('id','name','logotipo'));
     }
 }
